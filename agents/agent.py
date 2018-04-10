973fcf61885eb9190d2f2ae9d217ed04b7e7c826
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-04-10 15:54:00 +0200

    feat: Initialize Actors and Critics

--- agents/agent.py
+++ agents/agent.py
@@ -15,9 +15,10 @@ class Agent():
         """
 
         # Environment configuration
-        self.state_size = cfg['env']['state_size']
-        self.action_size = cfg['env']['action_size']
+        self.state_size = cfg['env']['state_size'][0]
+        self.action_size = cfg['env']['action_size'][0]
         self.action_low, self.action_high = cfg['env']['action_range']
+
 
         # Actor Model
         self.actor_local = Actor(self.state_size, self.action_size, self.action_low, self.action_high, cfg['actor'])
@@ -28,6 +29,8 @@ class Agent():
         self.critic_target = Critic(self.state_size, self.action_size, cfg['critic'])
 
         # Initialize target model
+        self.actor_target.model.set_weights(self.actor_local.model.get_weights())
+        self.critic_target.model.set_weights(self.critic_local.model.get_weights())
 
         # Replay memory
         self.memory = ReplayBuffer(**cfg['agent']['memory'])
