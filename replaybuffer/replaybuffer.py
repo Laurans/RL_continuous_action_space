a06ec03b2440d5e19243851a2cca953e74ace7cd
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-04-16 13:32:29 +0200

    Feat: Review ReplayBuffer to get past experiences

--- replaybuffer/replaybuffer.py
+++ replaybuffer/replaybuffer.py
@@ -1,35 +1,66 @@
-import random
-from collections import namedtuple, deque
+import numpy as np
 
 class ReplayBuffer():
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, buffer_size, batch_size):
-        """Initialize a ReplayBuffer object.
-        Params
-        ======
-            buffer_size: maximum size of buffer
-            batch_size: size of each training batch
+    def __init__(self, buffer_size, batch_size, past_size):
         """
-        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        Initialize a ReplayBuffer object.
+        :param buffer_size: maximum size of buffer
+        :param batch_size: size of each training batch
+        :param past_size: number of past experiences to pack with the current experience for training
+        """
+        self.past_size = past_size
+        self.max_size = buffer_size
         self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+        self.insert_index = 0
+
+        experience_type = [('state', (np.float32, 2)), ('action', np.float32), ('reward', np.float32),
+                           ('next_state', (np.float32, 2)), ('done', np.int32)]
+
+        self.memory = np.zeros(buffer_size, dtype=experience_type)
 
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
+        assert state is not None, 'State cannot be None'
+        assert action is not None, 'Action cannot be None'
+        assert reward is not None, 'Reward cannot be None'
+        assert next_state is not None, 'State 2 cannot be None'
+
+        experience = (state.copy(), action, reward, next_state.copy(), done)
+
+        if self.insert_index == self.max_size:
+            # Roll the memory to overwrite the oldest experience
+            self.memory = np.roll(self.memory, -1, axis=0)
+            index = -1
+        else:
+            index = self.insert_index
+
+        self.memory[index] = experience
+
+        # Increment until it is at full capacity
+        if self.insert_index < self.max_size:
+            self.insert_index += 1
+
+    def get(self, index):
+        """Get an experience with its past"""
+        assert index < self.insert_index
+        assert index >= self.past_size
+
+        return self.memory[index - self.past_size: index + 1]
 
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
-        return random.sample(self.memory, k=self.batch_size)
+        indexes = np.random.randint(self.past_size, self.insert_index, self.batch_size)
+
+        batch = np.vstack([self.get(i) for i in indexes])
+        return batch
 
     def __len__(self):
         """Return the current size of internal memory."""
-        return len(self.memory)
+        return self.insert_index
 
     def is_sufficient(self):
-        """
-        :return: True if there is sufficient experience stocked
-        """
-        return len(self.memory) > self.batch_size
+        """Return True if we can start sampling"""
+        return self.insert_index > self.batch_size + self.past_size
