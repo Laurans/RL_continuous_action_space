c25df3f5928f0ee3c8a917c98c5981a6e575aa6b
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-04-19 13:20:35 +0200

    fix: Add reshape in ReplayBuffer to be consistant

--- utils/replaybuffer.py
+++ utils/replaybuffer.py
@@ -1,9 +1,10 @@
 import numpy as np
+import sys
 
 class ReplayBuffer():
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, buffer_size, batch_size, past_size):
+    def __init__(self, buffer_size, batch_size, past_size, state_shape):
         """
         Initialize a ReplayBuffer object.
         :param buffer_size: maximum size of buffer
@@ -13,16 +14,21 @@ class ReplayBuffer():
         self.past_size = past_size
         self.max_size = buffer_size
         self.batch_size = batch_size
+        self.state_shape = state_shape
+        self.window_state_shape = [past_size + 1] + state_shape
 
         self.insert_index = 0
 
-        experience_type = [('state', (np.float32, 2)), ('action', np.float32), ('reward', np.float32),
-                           ('next_state', (np.float32, 2)), ('done', np.int32)]
+        experience_type = [('state', (np.float32, self.state_shape)),
+                           ('action', np.float32),
+                           ('reward', np.float32),
+                           ('next_state', (np.float32, self.state_shape)),
+                           ('done', np.int32)]
 
-        self.batch_experience_type = [('state', (np.float32, (past_size+1,2))),
+        self.batch_experience_type = [('state', (np.float32, self.window_state_shape)),
                                       ('action', np.float32),
                                       ('reward', np.float32),
-                                      ('next_state', (np.float32, (past_size+1,2))),
+                                      ('next_state', (np.float32, self.window_state_shape)),
                                       ('done', np.int32)]
 
         self.memory = np.zeros(buffer_size, dtype=experience_type)
@@ -34,7 +40,9 @@ class ReplayBuffer():
         assert reward is not None, 'Reward cannot be None'
         assert next_state is not None, 'State 2 cannot be None'
 
-        experience = (state.copy(), action, reward, next_state.copy(), done)
+        experience = (state.copy().reshape(self.state_shape),
+                      action, reward,
+                      next_state.copy().reshape(self.state_shape), done)
 
         if self.insert_index == self.max_size:
             # Roll the memory to overwrite the oldest experience
@@ -51,7 +59,6 @@ class ReplayBuffer():
 
     def get(self, index):
         """Get an experience with its past"""
-        assert index < self.insert_index
         assert index >= self.past_size
 
         experience = self.memory[index - self.past_size: index + 1]
@@ -65,9 +72,23 @@ class ReplayBuffer():
 
         return new_experience
 
+    def get_last_past_states(self, state):
+        state = state.reshape(self.state_shape)
+
+        index = self.insert_index
+        experience = self.memory[index - self.past_size: index + 1]
+        past_states = np.vstack(experience['state'])
+
+        if list(past_states.shape) != self.window_state_shape:
+            past_states = np.vstack([past_states, state])
+        else:
+            past_states[-1] = state
+
+        return np.expand_dims(past_states, axis=0)
+
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
-        indexes = np.random.randint(self.past_size, self.insert_index, self.batch_size)
+        indexes = np.random.choice(np.arange(self.past_size, self.insert_index), size=self.batch_size, replace=False)
 
         batch = np.vstack([self.get(i) for i in indexes])
         return batch
