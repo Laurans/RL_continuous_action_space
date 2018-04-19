33da403af5c5ccaf26d4472ccd8ec32b378d758d
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-04-19 13:27:36 +0200

    feat: Populate the experience memory + render

--- Monitor.py
+++ Monitor.py
@@ -23,6 +23,25 @@ def interact(env, agent, num_episodes=20000, window=100):
     # initialize best average reward
     best_avg_reward = -math.inf
     # initialize monitor for most recent rewards
+
+    # Random episode
+    for i_episode in range(1, num_episodes//2+1):
+        state = env.reset()
+        while True:
+            action = env.action_space.sample()
+            next_state, reward, done, _ = env.step(action)
+
+
+            if done:
+                next_state = np.zeros(state.shape)
+                agent.memory.add(state, action, reward, next_state, done)
+                break
+
+            agent.memory.add(state, action, reward, next_state, done)
+            state = next_state
+        print("\rEpisode {}/{} || RANDOM ".format(i_episode, num_episodes//2),end="\t")
+        sys.stdout.flush()
+
     samp_rewards = deque(maxlen=window)
     # for each episode
     for i_episode in range(1, num_episodes + 1):
@@ -31,6 +50,7 @@ def interact(env, agent, num_episodes=20000, window=100):
         # initialize the sampled reward
         samp_reward = 0
         while True:
+            env.render()
             # agent selects an action
             action = agent.act(state)
             # agent performs the selected action
@@ -54,7 +74,7 @@ def interact(env, agent, num_episodes=20000, window=100):
             if avg_reward > best_avg_reward:
                 best_avg_reward = avg_reward
         # monitor progress
-        print("\rEpisode {}/{} || Best average reward {} , last samp reward {}".format(i_episode, num_episodes, best_avg_reward, samp_reward), end="")
+        print("\rEpisode {}/{} || Best average reward {} , last samp reward {}".format(i_episode, num_episodes, best_avg_reward, samp_reward), end="\t")
         sys.stdout.flush()
         if i_episode == num_episodes: print('\n')
     return avg_rewards, best_avg_reward
