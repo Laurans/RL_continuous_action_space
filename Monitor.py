2dda65f46232b89ad62dcc495b56219d7c47ba93
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-04-20 13:22:32 +0200

    feat: change config+add summaries

--- Monitor.py
+++ Monitor.py
@@ -46,12 +46,15 @@ def interact(env, agent, saver, sess, writer, num_episodes=20000, window=100, nu
 
     samp_rewards = deque(maxlen=window)
     step = 0
+    reward_log = tf.Summary()
+    loss_log = tf.Summary()
     # for each episode
     for i_episode in range(1, num_episodes + 1):
         # begin the episode
         state = env.reset()
         # initialize the sampled reward
         samp_reward = 0
+        samp_loss = []
         while True:
             step += 1
             if render:
@@ -61,9 +64,9 @@ def interact(env, agent, saver, sess, writer, num_episodes=20000, window=100, nu
             # agent performs the selected action
             next_state, reward, done, _ = env.step(action)
             # agent performs internal updates based on sampled experience
-            summaries = agent.step(action, reward, next_state, done)
-            if summaries is not None:
-                writer.add_summary(summaries, step)
+            loss = agent.step(action, reward, next_state, done)
+            if loss is not None:
+                samp_loss.append(loss)
             # update the sampled reward
             samp_reward += reward
             # update the state (s <- s') to next time step
@@ -71,6 +74,12 @@ def interact(env, agent, saver, sess, writer, num_episodes=20000, window=100, nu
             if done:
                 # save final sampled reward
                 samp_rewards.append(samp_reward)
+
+                loss_log.value.add(tag='mean_episode_loss', simple_value=np.mean(samp_loss))
+                reward_log.value.add(tag='episode_rewards', simple_value=samp_reward)
+                writer.add_summary(loss_log, i_episode)
+                writer.add_summary(reward_log, i_episode)
+
                 break
         if (i_episode >= 100):
             # get average reward from last 100 episodes
